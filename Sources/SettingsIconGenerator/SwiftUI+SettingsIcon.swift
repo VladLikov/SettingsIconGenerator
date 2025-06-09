6d369f87bf370fec055270e8c24a2e589ff9f4c4
Vlad Likov <vlad.lykov.2016@yandex.ru>
2025-06-09 13:52:40 +0300

    Update SwiftUI+SettingsIcon.swift

--- Sources/SettingsIconGenerator/SwiftUI+SettingsIcon.swift
+++ Sources/SettingsIconGenerator/SwiftUI+SettingsIcon.swift
@@ -1,18 +1,19 @@
 #if canImport(SwiftUI)
 import SwiftUI
+import SafeSFSymbols
 
 @available(iOS 13, tvOS 13, macOS 11, *)
 public struct SettingsIcon: View {
 
-    let systemName: String
+    let symbol: SafeSFSymbol
     let backgroundColor: Color
 
     public var body: some View {
         ZStack {
-            Image(systemName: backgroundSystemName)
+            Image(backgroundSymbol)
                 .font(.system(size: CGFloat(backgroundFontSize)))
                 .foregroundColor(backgroundColor)
-            Image(systemName: systemName)
+            Image(symbol)
                 .font(.system(size: CGFloat(iconFontSize)))
                 .foregroundColor(.white)
         }
