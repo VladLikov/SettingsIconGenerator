c49f2a3a3ef6fe7457a555eaac346d84853068c9
Vlad Likov <vlad.lykov.2016@yandex.ru>
2025-09-06 21:57:37 +0300

    Update UIImageExtensions.swift

--- Sources/SettingsIconGenerator/UIImageExtensions.swift
+++ Sources/SettingsIconGenerator/UIImageExtensions.swift
@@ -31,5 +31,33 @@ extension UIImage {
             
         }
     }
+
+        @available(iOS 13, tvOS 13, *)
+    public static func generateSettingsIcon(_ image: UIImage, backgroundColor: UIColor) -> UIImage {
+
+        let iconConfig       = UIImage.SymbolConfiguration(pointSize: CGFloat(iconFontSize),       weight: .regular)
+        let backgroundConfig = UIImage.SymbolConfiguration(pointSize: CGFloat(backgroundFontSize), weight: .regular)
+
+        let icon = image
+            .withConfiguration(iconConfig)
+            .withTintColor(.white, renderingMode: .alwaysOriginal)
+
+        let background = UIImage(backgroundSymbol)
+            .withConfiguration(backgroundConfig)
+            .withTintColor(backgroundColor, renderingMode: .alwaysOriginal)
+
+        let renderer = UIGraphicsImageRenderer(size: background.size)
+        return renderer.image { _ in
+            background.draw(in: CGRect(origin: .zero, size: background.size))
+            let rect = CGRect(
+                x: (background.size.width  - icon.size.width)  / 2,
+                y: (background.size.height - icon.size.height) / 2,
+                width:  icon.size.width,
+                height: icon.size.height
+            )
+            icon.draw(in: rect)
+            
+        }
+    }
 }
 #endif
